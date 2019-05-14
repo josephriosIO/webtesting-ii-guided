1c0db70690f2762787a657661088462e95b0cb71
josephriosio <blankworldwide@gmail.com>
2019-05-14 10:24:19 -0700

    added hook stuff with testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "react-testing-library/cleanup-after-each";
+import { render, fireEvent } from "react-testing-library";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App />", () => {
+  it("renders fine", () => {
+    render(<App />);
+  });
+
+  it("renders hello world", () => {
+    const { getByText } = render(<App />);
+    getByText(/hello world!/i);
+  });
+
+  it("should greet the team", () => {
+    const { getByText } = render(<App />);
+
+    const button = getByText(/greet/i);
+    fireEvent.click(button);
+
+    getByText(/hello web 18/i);
+  });
 });
